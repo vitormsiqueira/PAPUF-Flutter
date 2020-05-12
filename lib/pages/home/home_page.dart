9a69c7e324ebd36c1b02de7396a9675a6f9a2c5b
vitormsiqueira <vmsvitor@outlook.com>
2020-05-12 02:36:29 -0400

    Set current class room

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -16,7 +16,8 @@ class _HomePageState extends State<HomePage> {
   bool pressed = false;
   int _selectedClass = 0;
   final _classOptions = [print("Sala 1")];
-
+  int currentClassRoom = 1;
+  
   // lista que gera a lista de salas presentes na tabBar, assim como algumas configurações
   final List<Tab> myTabs = List.generate(
     15,
@@ -72,7 +73,12 @@ class _HomePageState extends State<HomePage> {
                     shape: BoxShape.circle,
                   ),
                   tabs: myTabs,
-                  
+                  onTap: (index){
+                    setState(() {
+                      print(index+1);
+                      currentClassRoom = index+1;
+                    });
+                  },
                 ),
               ),
             ),
@@ -82,156 +88,53 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
 
-_body(BuildContext context) {
-  return Container(
-    color: Colors.white,
-    height: MediaQuery.of(context).size.height, //Add a full heigh white container 
-    child: SingleChildScrollView(
-        padding: EdgeInsets.only(bottom: 30),
-        //height: MediaQuery.of(context).size.height, // Permite expandir para toda a tela na altura
-        child: Column(
-          children: <Widget>[
-            _textControle(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center, // centraliza os controles
-              children: <Widget>[
-                Controle(ar1, "temp-1", currentSala: 1),
-                SizedBox(width: 40,),
-                Controle(ar2, "temp-2", currentSala: 1),
-              ],
-            ),
-            
-            // _textDashboard(),
-            // _dashboard(context),
-          ],
-        ),
-      ),
-  );
-}
 
-// _dashboard(BuildContext context) {
-//   return Padding(
-//     padding: const EdgeInsets.only(bottom: 30.0, left: 30, right: 30),
-//     child: Container(
-//       padding: EdgeInsets.only(top: 10, bottom: 10.0, left: 10, right: 10),
-//       child: Column(
-//         children: <Widget>[
-//           Row(
-//             children: <Widget>[
-//               Container(
-//                 width: 60,
-//                 height: 60,
-//                 child: Icon(
-//                   Icons.trending_up,
-//                   color: hexToColor("#4163CD"),
-//                   size: 35,
-//                 ),
-//               ),
-//               Container(
-//                 padding: EdgeInsets.only(left: 15), //consertar isso daqui
-//                 child: Row(
-//                   children: <Widget>[
-//                     Container(
-//                       child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: <Widget>[
-//                         Text(
-//                           "Consumo hoje",
-//                           style: TextStyle(
-//                               color: Colors.black54,
-//                               fontSize: 20,
-//                               fontWeight: FontWeight.w800),
-//                         ),
-//                         SizedBox(
-//                           height: 5,
-//                         ),
-//                         Text(
-//                           "450 kW",
-//                           style: TextStyle(
-//                               color: Colors.black45,
-//                               fontSize: 16,
-//                               fontWeight: FontWeight.w600),
-//                         ),
-//                       ],
-//                       ),
-//                     )
-//                   ],
-//                 ),
-//               ),
-//               Container(
-//                 height: 55,
-//                 child: RawMaterialButton(
-//                   onPressed: () {},
-//                   child: Icon(
-//                     Icons.arrow_forward,
-//                     color: Colors.black45,
-//                     size: 30,
-//                   ),
-//                   shape: CircleBorder(),
-//                 ),
-//               )
-//             ],
-//           ),
-          
-//         ],
-//       ),
-//       width: MediaQuery.of(context).size.width,
-//       height: 80,
-//       decoration: BoxDecoration(
-//         borderRadius: BorderRadius.circular(20),
-//         color: Colors.white,
-//         boxShadow: [
-//           BoxShadow(
-//             color: Colors.black26,
-//             blurRadius: 15.0, // has the effect of softening the shadow
-//             spreadRadius: .5, // has the effect of extending the shadow
-//           )
-//         ],
-//       ),
-//     ),
-//   );
-// }
-
-// _textDashboard() {
-//   return Row(
-//     crossAxisAlignment: CrossAxisAlignment.start,
-//     children: <Widget>[
-//       Container(
-//         child: Padding(
-//           padding: const EdgeInsets.only(left: 30.0, top: 30, bottom: 20),
-//           child: Text(
-//             "Dashboard",
-//             style: TextStyle(
-//               color: Colors.black,
-//               fontSize: 24,
-//               fontWeight: FontWeight.bold,
-//             ),
-//           ),
-//         ),
-//       )
-//     ],
-//   );
-// }
-
-_textControle() {
-  return Row(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Container(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 30.0, top: 30, bottom: 20),
-          child: Text(
-            "Controle | Sala ",
-            style: TextStyle(
-              color: Colors.black54,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
+  _body(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      height: MediaQuery.of(context).size.height, //Add a full heigh white container 
+      child: SingleChildScrollView(
+          padding: EdgeInsets.only(bottom: 30),
+          //height: MediaQuery.of(context).size.height, // Permite expandir para toda a tela na altura
+          child: Column(
+            children: <Widget>[
+              _textControle(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center, // centraliza os controles
+                children: <Widget>[
+                  Controle(ar1, "temp-1", currentSala: currentClassRoom),
+                  SizedBox(width: 40,),
+                  Controle(ar2, "temp-2", currentSala: currentClassRoom),
+                ],
+              ),
+              
+              // _textDashboard(),
+              // _dashboard(context),
+            ],
           ),
         ),
-      )
-    ],
-  );
+    );
+  }
+
+  _textControle() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 30.0, top: 30, bottom: 20),
+            child: Text(
+              "Controle | Sala $currentClassRoom",
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
 }
